49af2d37092368f8f7c78b17e1be4594781dadc7
carlosarantes21 <cadusantosarantes@gmail.com>
2026-04-15 16:11:32 -0300

    added option to choose when to go back to the start(before the screen would get spammed with the initial options)

--- src/Main.java
+++ src/Main.java
@@ -3,84 +3,94 @@ import java.util.Scanner;
 public class Main{
     static void main(String[] args) throws InterruptedException {
 
-        int choice, balance=0,deposit,withdraw;
+        int choice, choice2;
+        double balance=0,deposit,withdraw;
         Scanner scan1 = new Scanner(System.in);
 
         do {
-            System.out.println("*******************");
-            System.out.println("  BANKING PROGRAM  ");
-            System.out.println("*******************");
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
             System.out.println("1. Show Balance\n" +
                     "2. Deposit\n" +
                     "3. Withdraw\n" +
                     "4. Exit");
-            System.out.println("*******************");
+            System.out.println("************************");
             System.out.println("Enter your choice (1-4):");
+            System.out.println("************************");
             choice = scan1.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("*******************");
-                    System.out.println("  CURRENT BALANCE: ");
-                    System.out.println("$" + balance);
-                    System.out.println("*******************");
-                    break;
-                case 2:
-                    System.out.println("*****************************");
-                    System.out.println("Type the amount of deposit: ");
-                    System.out.println("*****************************");
-                    deposit = scan1.nextInt();
-                        if(deposit>0){
-                    balance += deposit;
-                    for (int i = 3; i > 0; i--) {
-                        System.out.println(i + "...");
-                        Thread.sleep(1000);
-                    }
-                    System.out.println("*****************");
-                    System.out.println("DEPOSIT CONFIRMED");
-                    System.out.println("*****************");
-                        }
-                        else{
+            choice2=1;
+            while(choice2!=2) {
+                switch (choice) {
+                    case 1:
+                        System.out.println("****************");
+                        System.out.println("CURRENT BALANCE: ");
+                        System.out.printf("$%.2f\n" , balance);
+                        System.out.println("****************");
+                        System.out.println("Type '2' to go back");
+                        choice2=scan1.nextInt();
+                        break;
+                    case 2:
+                        System.out.println("***************************");
+                        System.out.println("Type the amount of deposit: ");
+                        System.out.println("***************************");
+                        deposit = scan1.nextDouble();
+                        if (deposit > 0) {
+                            balance += deposit;
+                            for (int i = 3; i > 0; i--) {
+                                System.out.println(i + "...");
+                                Thread.sleep(1000);
+                            }
+                            System.out.println("*****************");
+                            System.out.println("DEPOSIT CONFIRMED");
+                            System.out.println("*****************");
+                            System.out.println("Type '2' to go back");
+                            choice2=scan1.nextInt();
+                        } else {
                             System.out.println("*************************");
                             System.out.println("INVALID AMOUNT, TRY AGAIN");
                             System.out.println("*************************");
+
                         }
-                    break;
-                case 3:
-                    System.out.println("*****************************");
-                    System.out.println("Type the amount of withdraw: ");
-                    System.out.println("*****************************");
-                    withdraw = scan1.nextInt();
-                        if(withdraw<=balance){
-                    balance -= withdraw;
-                    System.out.println("CHECKING BALANCE");
-                    for (int i = 3; i > 0; i--) {
-                        System.out.println(i + "...");
-                        Thread.sleep(1000);
-                    }
-                    System.out.println("************************");
-                    System.out.println("   WITHDRAW CONFIRMED   ");
-                    System.out.println("************************");
-                        }
-                        else {
+                        break;
+                    case 3:
+                        System.out.println("****************************");
+                        System.out.println("Type the amount of withdraw: ");
+                        System.out.println("****************************");
+                        withdraw = scan1.nextDouble();
+                        if (withdraw <= balance) {
+                            balance -= withdraw;
                             System.out.println("CHECKING BALANCE");
                             for (int i = 3; i > 0; i--) {
                                 System.out.println(i + "...");
                                 Thread.sleep(1000);
                             }
-                            System.out.println("************************");
-                            System.out.println("   NOT ENOUGH BALANCE   ");
-                            System.out.println("************************");
+                            System.out.println("******************");
+                            System.out.println("WITHDRAW CONFIRMED");
+                            System.out.println("******************");
+                            System.out.println("Type '2' to go back");
+                            choice2=scan1.nextInt();
+                        } else {
+                            System.out.println("CHECKING BALANCE");
+                            for (int i = 3; i > 0; i--) {
+                                System.out.println(i + "...");
+                                Thread.sleep(1000);
+                            }
+                            System.out.println("******************");
+                            System.out.println("NOT ENOUGH BALANCE");
+                            System.out.println("******************");
                         }
-                    break;
-                case 4:
-                    System.out.println("*******************");
-                    System.out.println("   SESSION ENDED   ");
-                    System.out.println("*******************");
-                    break;
+                        break;
+                    case 4:
+                        System.out.println("*************");
+                        System.out.println("SESSION ENDED");
+                        System.out.println("*************");
+                        choice2=2;
+                        break;
+                }
             }
         }while(choice!=4);
     }
 }
 
 //TO DO: ADD OPTION TO GO BACK OR END INSTEAD OF SHOWING ALL 4 OPTIONS AGAIN
-//TO DO: MAKE IT IMPOSSIBLE TO WITHDRAW MORE THAN YOU HAVE
